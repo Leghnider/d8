70a40053515e9718fd2a7e1404c0c423e01b346a
Alice Lui <aytlui@gmail.com>
2021-05-06 13:16:39 +0100

    can add user profiles to database

--- routes/home.js
+++ routes/home.js
@@ -6,11 +6,15 @@ var HomeController = require("../controllers/home");
 router.get('/', HomeController.Index);
 router.get('/register', HomeController.Register);
 router.post('/register', HomeController.CreateAccount);
+router.get('/register-profile', HomeController.RegisterProfile);
+router.post('/register-profile', HomeController.CreateProfile);
 router.get('/login', HomeController.Login);
+router.get('/home', HomeController.Dashboard);
+
 /*
 router.get('/logout', HomeController.Logout);
 router.get('/register-profile', HomeController.RegisterAccount); 
-router.get('/home', HomeController.Dashboard);
+
 */
 
 
