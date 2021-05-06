8dccfac4cc09bfef96d3f0af8a71ab5436cdc673
Precieuse Hali <Precieusehali93x@hotmail.co.uk>
2021-05-06 11:17:03 +0100

    Sign up feature

--- controllers/home.js
+++ controllers/home.js
@@ -1,7 +1,34 @@
+var User = require('../models/userAccount')
+const bcrypt = require('bcrypt');
+
 var HomeController = {
   Index: function(req, res) {
     res.render('home/index', { title: 'd8' });
-  }
-};
+  },
+
+  Register: function (req, res) {
+		res.render("home/register", {title: 'Register'});
+	},
+	CreateAccount: async function (req, res) {
+		const { firstName, lastName, age, email, password } = req.body;
+      const hash = await bcrypt.hash(password, 12);
+    
+			var user = new User({
+        firstName,
+        lastName,
+        age,
+				email,
+				password: hash,
+			});
+
+      await user.save(function(err){
+        if (err) { 
+          throw err 
+        }
+      res.status(201).redirect('/');
+     });
+    }
+  };
+
 
 module.exports = HomeController;
--- routes/home.js
+++ routes/home.js
@@ -4,11 +4,13 @@ var router = express.Router();
 var HomeController = require('../controllers/home');
 
 router.get('/', HomeController.Index);
+router.get('/register', HomeController.Register);
+router.post('/register', HomeController.CreateAccount);
 
 /*
 router.get('/login', HomeController.Login);
 router.get('/logout', HomeController.Logout);
-router.get('/register-account', HomeController.RegisterAccount);
+
 router.get('/register-profile', HomeController.RegisterAccount); 
 router.get('/home', HomeController.Dashboard);
 */
