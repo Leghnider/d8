d8efe85955c2378ce595ebc679be2b64fbebdc5f
Zineb <32713252+Leghnider@users.noreply.github.com>
2021-05-10 08:44:42 +0100

    update to remove google keys

--- passport.js
+++ passport.js
@@ -10,8 +10,8 @@ passport.deserializeUser(function(id, done) {
 });
 
 passport.use(new GoogleStrategy({
-    clientID: "328200141523-aqo6l3tkvf8k3mqg3r7rsrp2s2sn0og9.apps.googleusercontent.com",
-    clientSecret: "J5_Jc78iHN7QZWuK7DBmfcSg",
+    clientID: "XXX",
+    clientSecret: "XXX",
     callbackURL: "http://localhost:3000/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
